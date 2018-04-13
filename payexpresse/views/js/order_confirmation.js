203d0a34f04e3cd056b58674f309622d3ca609e3
pape samba ndour <papesambandour@hotmail.com>
2018-04-13 13:53:10 +0000

    add plugin

---
+++ payexpresse/views/js/order_confirmation.js
@@ -0,0 +1,9 @@
+/**
+ * Created by PhpStorm.
+ * User: PAPE SAMBA NDOUR : 777293282; papesambandour@hotmail.com
+ * Date: 05/04/2018
+ * Time: 13:09
+ */
+$(document).ready(function() {
+    $('.page-order-confirmation #order-details ul').append($('#payexpresse_transaction_id'));
+});
